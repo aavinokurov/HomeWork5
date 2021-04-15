fa6fbe760ec3a4bdd9414e60ebe0ab098ac0d36a
aavinokurov <aavinokurov@outlook.com>
2021-04-15 17:51:26 +0300

    Finish exercise 3, 4

---
+++ HomeWork5/Exercise3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NoRepetition("ПППОООГГГООООДДДААА"));
+            Console.WriteLine(NoRepetition("Ххххоооорррооошшшиий деееннннь"));
+        }
+
+        /// <summary>
+        /// Метод возвращает новую строку, в которой отсутствуют все повторяющиеся, рядом стоящие символы
+        /// </summary>
+        /// <param name="text">Произвольный текст в виде строки</param>
+        /// <returns>Возвращает новую строку, в которой отсутствуют все повторяющиеся, рядом стоящие символы.</returns>
+        public static string NoRepetition (string text)
+        {
+            string result = "";
+
+            if (!string.IsNullOrEmpty(text))
+            {
+                text = text.ToLower();
+
+                result += text[0];
+
+                for (int i = 1; i < text.Length; i++)
+                {
+                    if (text[i - 1] != text[i])
+                    {
+                        result += text[i];
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ HomeWork5/Exercise4/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsProgression(new int[] { 1,2,4,8,16}));
+        }
+
+        /// <summary>
+        /// Метод определяет, являются ли числа в массиве арифметической или геометрической прогрессией или массив не содержит прогрессии
+        /// </summary>
+        /// <param name="num">Массив чисел</param>
+        /// <returns>Результат в виде строки</returns>
+        public static string IsProgression (params int[] num)
+        {
+            string result = "";
+
+            if (num != null)
+            {
+                bool isArithmetic = true;
+                bool isGeometric = true;
+
+                if (num.Length > 1)
+                {
+                    int a = num[1] - num[0];
+                    double g = (double)num[1] / num[0];
+
+                    for (int i = 2; i < num.Length; i++)
+                    {
+                        if (num[i] - num[i - 1] != a)
+                        {
+                            isArithmetic = false;
+                        }
+
+                        if ((double)num[i]/ num[i - 1] != g)
+                        {
+                            isGeometric = false;
+                        }
+
+                        if (!isArithmetic && !isGeometric)
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    isArithmetic = false;
+                    isGeometric = false;
+                }
+
+                if (isArithmetic) { result = "Арифметическая прогрессия"; }
+                else if (isGeometric) { result = "Геометрическая прогрессия"; }
+                else { result = "Не являются прогрессией"; }
+
+            }
+
+            return result;
+        }
+    }
+}
